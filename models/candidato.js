f618b41ff0cc7855c0890121871e6eb20701f846
Gonzalo <gmarquez@mexerp.com>
2017-02-14 18:23:28 -0600

    segmented candidato model into 2 parts (candidato, and candidato_detalle)

--- models/candidato.js
+++ models/candidato.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ExperienciaLaboralSchema = require('./experiencia_laboral');
-const EducacionSchema = require('./educacion');
-const ConocimientosSchema = require('./conocimientos');
-const IdiomaSchema = require('./idioma');
 const mongooseUniqueValidator = require('mongoose-unique-validator');
 
 const Candidato = new Schema({
@@ -11,22 +7,6 @@ const Candidato = new Schema({
     password: {type: String, required: true},
     name: {type: String, required: true},
     lastname: {type: String, required: true},
-    gender: {type: String, enum: ['m', 'f'], default: 'm'},
-    city: String,
-    zip: {type: String, maxlength: 5},
-    state: String,
-    country: String,
-    birthdate: Date,
-    nationality: String,
-    phone: String,
-    created: Date,
-    updated: Date,
-    experience: [ExperienciaLaboralSchema],
-    education: [EducacionSchema],
-    languages: [IdiomaSchema],
-    skills: [ConocimientosSchema],
-    status: {type: String, enum: ['active', 'inactive'], default: 'active'},
-    scope: {type: String, default: 'candidato'}
 });
 
 schema.plugin(mongooseUniqueValidator);
