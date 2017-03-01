eb619b853763a292785ed396fa6cb7f3ae749f08
Gonzalo Marquez <gmarquez@mexnetwork.com>
2017-02-28 22:34:18 -0600

    updated candidato model

--- models/candidato.js
+++ models/candidato.js
@@ -4,9 +4,25 @@ const mongooseUniqueValidator = require('mongoose-unique-validator');
 
 const Candidato = new Schema({
     email: {type: String, required: true, unique: true, lowercase: true},
-    password: {type: String, required: true},
-    name: {type: String, required: true},
-    lastname: {type: String, required: true},
+    name: String,
+    lastname: String,
+    gender: {type: String, enum: ['m', 'f']},
+    city: String,
+    zip: {type: String, maxlength: 5},
+    state: String,
+    country: String,
+    birthdate: Date,
+    nationality: String,
+    phone: String,
+    secondary_phone: String,
+    created: Date,
+    updated: Date,
+    experience: [ExperienciaLaboralSchema],
+    education: [EducacionSchema],
+    languages: [IdiomaSchema],
+    skills: [ConocimientosSchema],
+    status: {type: String, enum: ['active', 'inactive'], default: 'active'},
+    scope: {type: String, default: 'candidato'}
 });
 
 schema.plugin(mongooseUniqueValidator);
