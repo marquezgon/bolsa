14179437e5f654181ff6806427ede736b7092695
Gonzalo <gmarquez@mexerp.com>
2017-03-16 15:48:07 -0600

    added salary field to experiencia laboral model

--- models/experiencia_laboral.js
+++ models/experiencia_laboral.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 const ExperienciaLaboralSchema = new Schema ({
     empresa: { type: String, required: [true, 'El nombre de la empresa es obligatorio'] },
     giro: { type: String, required: [true, 'El giro de la empresa es obligatorio'] },
-    area: { type: String, required: [true, 'El área dentro de la empresa es obligatorio'] },
     puesto: { type: String, required: [true, 'El puesto dentro de la empresa es obligatorio'] },
     inicio: {type: Date, required: [true, 'La fecha de inicio es obligatoria'] },
     termino: {type: Date },
     logros: { type: String },
+    salario: { type: string }
     sigue_laborando: { type: Boolean, default: false }
 });
 
